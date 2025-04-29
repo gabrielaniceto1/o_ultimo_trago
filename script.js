03250cbbeb180ede196211761ec2e84bf84b622d
Gabriel Aniceto <gabriel.aniceto@hotmail.com>
2025-04-29 02:53:17 -0300

    melhorei alguns aspectos da pagina

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Página de vendas carregada.");
+ /* slider dos depoimentos */
+  let currentSlide = 0;
+  const slides = document.querySelectorAll('.slide');
+  const totalSlides = slides.length;
+
+  document.querySelector('.next').addEventListener('click', () => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % totalSlides;
+    slides[currentSlide].classList.add('active');
+  });
+
+  document.querySelector('.prev').addEventListener('click', () => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    slides[currentSlide].classList.add('active');
+  });
+
